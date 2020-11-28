110da19adaf73bb84f6a7701feb53f98765080ef
Zefeng Wang <zefengwang@Zefengs-MacBook-Pro.local>
2020-11-28 19:06:45 +0800

    add 4hc3 as7

--- 4hc3/as7/js/index.js
+++ 4hc3/as7/js/index.js
@@ -1,20 +1,16 @@
-// run the code when the document has finished loading
-window.onload = function() {
+window.onload = function () {
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext("2d");
+    let user_hits = 0;  // keep track of the number of times the player ball collides with enemy
+    let border_hits = 0;
+    const threshold = 20;
 
-    // get the canvas element
-    var canvas = document.getElementById("canvas");
+    function random_y(v) {
+        return (600 - 2 * v) * Math.random() + v;
+    }
 
-    // get the context of the canvas, i.e. the object that allows us to draw
-    var c = canvas.getContext("2d");
-
-    // keep track of the number of times the player ball collides with enemy
-    var hits = 0;
-
-    // use a ball class to create ball objects... reduces code duplication
-    class ball {
-
-        // create a ball object
-        constructor(x,y,vx,vy,bvx,bvy,radius,color) {
+    class Ball {
+        constructor(x, y, vx, vy, bvx, bvy, radius, color, role) {
             this.x = x;  // x position
             this.y = y;  // y position
             this.vx = vx; // x velocity
@@ -23,14 +19,14 @@ window.onload = function() {
             this.bvy = bvy; // y velocity after bounce = vy * bvy
             this.radius = radius;
             this.color = color;
+            this.role = role;
         }
 
-        // draw the ball to the canvas
         draw() {
-            c.beginPath();
-            c.fillStyle = this.color;
-            c.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-            c.fill();
+            context.beginPath();
+            context.fillStyle = this.color;
+            context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+            context.fill();
         }
 
         // move the ball according to the velocity
@@ -42,6 +38,12 @@ window.onload = function() {
         // determine if the ball has hit a boundary, multiply it by the
         // bounce values for the desired effect
         boundary_check() {
+            if (this.role === 'enemy' && (this.x - this.radius) + this.vx < 0) {
+                border_hits++;
+                this.x = 1200 - this.radius;
+                this.y = random_y(this.radius);
+            }
+
             if ((this.y + this.radius) + this.vy > canvas.height ||
                 (this.y - this.radius) + this.vy < 0) {
                 this.vy = this.bvy * this.vy;
@@ -53,89 +55,108 @@ window.onload = function() {
         }
     }
 
-    // create the player ball
-    var player_ball = new ball(25,250,0,0,0,0,25,'green');
-
-    // create the enemy balls
-    var num_enemies = 3;
-    var enemy_balls = [];
-    for (var i = 0; i < num_enemies; i++)
-    {
-        var xdir = Math.random() > 0.50 ? 1 : -1;
-        var ydir = Math.random() > 0.50 ? 1 : -1;
-        var vx = xdir * Math.floor(4 + (Math.random() * 5));
-        var vy = ydir * Math.floor(4 + (Math.random() * 5));
-        enemy_balls[i] =
-            new ball(canvas.width-50,50 + (i*50),vx,vy,-1,-1,25,'red');
-    }
+    let player_ball = new Ball(25, 250, 0, 0, 0, 0, 25, 'green', 'player');
+    let enemy_ball = new Ball(canvas.width - 50, random_y(25), -8, 5 * Math.random() + 3, -1, -1, 25, 'red', 'enemy');
 
     // change the direction of the ball based on the arrow key input
-    document.onkeydown = function(e) {
-        console.log(e.code);
-        if (e.code == "ArrowDown") player_ball.vy = 5;
-        else if (e.code == "ArrowUp") player_ball.vy = -5;
-        else if (e.code == "ArrowRight") player_ball.vx = 5;
-        else if (e.code == "ArrowLeft") player_ball.vx = -5;
-    };
+    // document.onkeydown = function (e) {
+    //     console.log(e.code);
+    //     if (e.code == "S") player_ball.vy = 5;
+    //     else if (e.code == "W") player_ball.vy = -5;
+    //     else if (e.code == "D") player_ball.vx = 5;
+    //     else if (e.code == "A") player_ball.vx = -5;
+    // };
+    //
+    // // when the player releases a key, stop the movement in that direction
+    // document.onkeyup = function (e) {
+    //     if (e.code == "S") player_ball.vy = 0;
+    //     else if (e.code == "W") player_ball.vy = 0;
+    //     else if (e.code == "D") player_ball.vx = 0;
+    //     else if (e.code == "A") player_ball.vx = 0;
+    // };
 
-    // when the player releases a key, stop the movement in that direction
-    document.onkeyup = function(e) {
-        if (e.code == "ArrowDown") player_ball.vy = 0;
-        else if (e.code == "ArrowUp") player_ball.vy = 0;
-        else if (e.code == "ArrowRight") player_ball.vx = 0;
-        else if (e.code == "ArrowLeft") player_ball.vx = 0;
-    };
+    let last_x = 0;
+    let last_y = 0;
 
-    // recogonizes and carries out any game logic, i.e. collisions
-    function game_logic()
-    {
-        // check if the balls have collided with a boundary
-        player_ball.boundary_check();
-        for (var i = 0; i < num_enemies; i++) enemy_balls[i].boundary_check();
+    /**
+     * touch screen event
+     */
+    function handleStart(e) {
+        last_x = e.touches[0].pageX;
+        last_y = e.touches[0].pageY;
+    }
 
-        // check if the player ball collides with any other balls
-        for (var i = 0; i < num_enemies; i++) {
-
-            // calculate the distance between the future ball positions
-            var dx = (enemy_balls[i].x + enemy_balls[i].vx) - (player_ball.x + player_ball.vx);
-            var dy = (enemy_balls[i].y + enemy_balls[i].vy) - (player_ball.y + player_ball.vy);
-            var distance = Math.sqrt(dx * dx + dy * dy);
-
-            // if a collision occurs, flip both ball's velocities
-            if (distance < enemy_balls[i].radius + player_ball.radius) {
-                hits++;
-                enemy_balls[i].vx = -enemy_balls[i].vx;
-                enemy_balls[i].vy = -enemy_balls[i].vy;
-            }
+    function handleMove(e) {
+        if (last_x - e.touches[0].pageX > threshold) { // move right
+            console.log("move right");
+            player_ball.vx = -5;
+        } else if (last_x - e.touches[0].pageX <= (-threshold)) { // move left
+            console.log("move left");
+            player_ball.vx = 5;
         }
+
+        if (last_y - e.touches[0].pageY > threshold) { // move up
+            console.log("move up")
+            player_ball.vy = -5;
+        } else if (last_y - e.touches[0].pageY <= (-threshold)) { // move down
+            console.log("move down")
+            player_ball.vy = 5;
+        }
+    }
+
+    function handleEnd(e) {
+        player_ball.vx = 0;
+        player_ball.vy = 0;
+    }
+
+
+    // recognizes and carries out any game logic, i.e. collisions
+    function game_logic() {
+        player_ball.boundary_check();
+        // check if the player ball collides with any other balls
+        // calculate the distance between the future ball positions
+        let dx = (enemy_ball.x + enemy_ball.vx) - (player_ball.x + player_ball.vx);
+        let dy = (enemy_ball.y + enemy_ball.vy) - (player_ball.y + player_ball.vy);
+        let distance = Math.sqrt(dx * dx + dy * dy);
+
+        // if a collision occurs
+        if (distance < enemy_ball.radius + player_ball.radius) {
+            user_hits++;
+            // enemy_ball.x = 600 - enemy_ball.radius;
+            // enemy_ball.y = (600 - 2 * this.radius) * Math.random() + this.radius;
+            enemy_ball = new Ball(canvas.width - 50, random_y(25), -8, 3 * Math.random() + 5, -1, -1, 25, 'red', 'enemy');
+
+        }
+
+        enemy_ball.boundary_check();
 
         // move the balls
         player_ball.move();
-        for (var i = 0; i < num_enemies; i++) enemy_balls[i].move();
-
+        enemy_ball.move();
     }
 
     // draws the game state to the canvas
-    function draw()
-    {
+    function draw() {
         // clear the canvas so that we start off with a blank canvas
-        c.clearRect(0,0,canvas.width,canvas.height);
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
         // draw the balls to the screen
         player_ball.draw();
-        for (var i = 0; i < num_enemies; i++) enemy_balls[i].draw();
+        enemy_ball.draw();
 
-        // draw the hits to the screen in the top-left corner
-        c.font = "20px serif";
-        c.fillText("Hits: " + hits, 5, 20);
+        // draw the user_hits to the screen in the top-left corner
+        context.font = "20px serif";
+        context.fillText("User hits: " + user_hits, 5, 20);
+        context.fillText("Border hits: " + border_hits, 5, 40);
     }
 
     // game loop runs every 30 milliseconds
-    setInterval(function() {
-
+    setInterval(function () {
         game_logic();
         draw();
+    }, 30);
 
-    } , 30);
-
+    canvas.ontouchstart = handleStart;
+    canvas.ontouchmove = handleMove;
+    canvas.ontouchend = handleEnd;
 }
